4ec8bd989dc29274fc3e26139d9946f1f23c2ece
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-15 20:08:39 +0200

    account for non-overwritten input evaluations

--- component.js
+++ component.js
@@ -24,6 +24,7 @@ export class Component {
       return {
         action: await this.action.component.exportAction(),
         evaluate: true,
+        type: "unify",
       };
     return await this.action.component.exportAction();
   }
