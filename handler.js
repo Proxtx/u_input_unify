745df6be83625ff909e0d7da397ebe6f6554b0d5
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-19 17:32:20 +0200

    Adjust to new apps dependency

--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
-import { api } from "../../public/meta.js";
 import config from "@proxtx/config";
+import { genModule } from "@proxtx/combine/combine.js";
+import { genCombine } from "@proxtx/combine-rest/request.js";
+
+let api = await genCombine(config.unifyGui, "public/api.js", genModule);
 
 export const evaluate = async (value) => {
   return await api.execute(
